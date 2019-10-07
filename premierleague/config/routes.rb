12d6028ac8c5bd18f7509285b630d86addcedbbd
Tony Le <tle68@academic.rrc.ca>
2019-10-07 01:13:00 -0500

    Created About page

--- premierleague/config/routes.rb
+++ premierleague/config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get '/about', to: 'about#about'
   get '/matchdates', to: 'match_dates#index'
   get 'match_dates/show'
   get 'match_dates/new'
